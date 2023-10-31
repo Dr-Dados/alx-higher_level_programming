2d44cd9eb7e97d2cdfede6cf97c5f85edb5e9cda
Dr-Dados <adnane.bachchar@gmail.com>
2023-10-31 19:30:03 +0000

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for n in range(122, 96, -1):
     if (n % 2 == 0):
-        print(chr(n), end="")
+        print("{}".format(chr(n)), end="")
     else:
-        print(chr(n - 32), end="")
+        print("{}".format(chr(n - 32)), end="")
