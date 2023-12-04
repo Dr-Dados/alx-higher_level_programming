300845bbc1efb9427c88d2d06b83b9e1e8b935d1
Dr-Dados <adnane.bachchar@gmail.com>
2023-12-04 14:19:19 +0000

    7-base_geometry.py

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -16,7 +16,7 @@ class BaseGeometry:
         :param name:
         :param value:
         """
-        if (type(value) != int):
-            raise TypeError(name + " must be an integer")
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
         if (value <= 0):
-            raise ValueError(name + " must be greater than 0")
+            raise ValueError(f"{name} must be greater than 0")
