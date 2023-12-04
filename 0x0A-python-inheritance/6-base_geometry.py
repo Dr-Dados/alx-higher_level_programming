bd303e3eff2febcb5da1c51750977fe2a415c45f
Dr-Dados <adnane.bachchar@gmail.com>
2023-12-04 13:48:23 +0000

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""BaseGeometry class"""
+
+
+class BaseGeometry:
+    """BaseGeometry"""
+    def area(self):
+        """area function
+        :param self:
+        """
+        raise Exception("area() is not implemented")
