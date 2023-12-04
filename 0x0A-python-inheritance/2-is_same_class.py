cd331fc309e67621a41cfd79740b9c04ad91e723
Dr-Dados <adnane.bachchar@gmail.com>
2023-12-04 11:51:03 +0000

    2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,5 +3,9 @@
 
 
 def is_same_class(obj, a_class):
-    """ isinstance function"""
+    """ isinstance function
+    
+    :param obj:
+    :param a_class:
+    """
     return isinstance(obj, a_class)
