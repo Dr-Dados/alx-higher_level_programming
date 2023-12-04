93a2f7d5e7386cd8760b19392863d7a8f62011ea
Dr-Dados <adnane.bachchar@gmail.com>
2023-12-04 12:01:08 +0000

    3-is_kind_of_class.py

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" iskind class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """iskind class
+
+    :param obj:
+    :param a_class:
+    """
+    return isinstance(obj, a_class)
