3ab6ca891b845fdfbd211f5fb8ecfd7929af63dd
Dr-Dados <adnane.bachchar@gmail.com>
2023-12-05 10:34:15 +0000

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-"""read file"""
+""" module """
 
 
 def read_file(filename=""):
-    """read file function
-
-    :param filename:
-    """
+    """ function """
     f = open(filename, 'r', encoding='utf-8')
     print(f.read(), end="")
     f.close()
