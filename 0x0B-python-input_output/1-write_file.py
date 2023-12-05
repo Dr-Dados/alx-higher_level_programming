0d21619d0818f7bcf1bc48741807212b6c581f23
Dr-Dados <adnane.bachchar@gmail.com>
2023-12-05 10:41:52 +0000

    write file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""module"""
+
+
+def write_file(filename="", text=""):
+    """ function """
+    with open(filename, 'w', encoding="utf-8") as f:
+        return(f.write(text))          
+ 
