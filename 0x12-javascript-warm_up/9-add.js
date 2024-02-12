0281d4ecacff4c773d737e67af9c882e6d8cf9ac
Dr-Dados <adnane.bachchar@gmail.com>
2024-02-12 08:56:53 +0000

    all remaining tasks

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-const { argv } = require('process');
-const a = parseInt(argv[2], 10);
-const b = parseInt(argv[3], 10);
+
 function add (a, b) {
-  return a + b;
+  console.log(a + b);
 }
-console.log(add(a, b));
+
+add(Number(process.argv[2]), Number(process.argv[3]));
