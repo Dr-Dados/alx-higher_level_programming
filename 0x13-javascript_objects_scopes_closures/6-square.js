a59c94b17e2e44cf44cef324cc4bd997c7c37270
Dr-Dados <adnane.bachchar@gmail.com>
2024-02-13 09:17:38 +0000

    6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,13 +3,10 @@
 const SquareBase = require('./5-square');
 
 class Square extends SquareBase {
-  constructor (size) {
-    super(size, size);
-  }
-
   charPrint (c) {
-    for (let i = 0; i < this.width; i++) {
-      console.log((!c ? 'X' : 'C').repeat(this.width));
+    if (!c) c = 'X';
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
     }
   }
 }
